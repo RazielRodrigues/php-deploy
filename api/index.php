3fa507098e36b4fc6eacf98dc3e4ac276302b1e7
Raziel Rodrigues <razielx3@live.com>
2024-09-01 16:40:19 +0100

    teste

--- api/index.php
+++ api/index.php
@@ -7,3 +7,21 @@ $key = openssl_pkey_new();
 var_dump($key);
 
 echo 'Raziel Rodrigues';
+
+?>
+
+<html>
+
+<button onclick="navigator.vibrate(30000)">Vibrate</button>
+<button onclick="navigator.vibrate([200, 100, 200])">Vibrate Long</button>
+
+
+<script>
+    if ("vibrate" in navigator) {
+        alert("Vibration API is supported");
+    } else {
+        alert("Vibration API is not supported");
+    }
+</script>
+
+</html>
